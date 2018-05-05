117e023a0234d040d9043830cbc16b7c0ea79e38
y500 <1248832396@qq.com>
2018-05-05 10:05:19 +0800

    AddTwoNumbers
    So,the code works
    改进了一下，不是用合成数字相加再转换的方法，而是链表之间直接相加的方法

--- AddTwoNumbers/AddTwoNumbers/AddTwoNumbers/AddTwoNumbers.cpp
+++ AddTwoNumbers/AddTwoNumbers/AddTwoNumbers/AddTwoNumbers.cpp
@@ -8,41 +8,80 @@ using namespace std;
 	ListNode(int x) : val(x), next(NULL) {};
 	
 };
-class Solution 
-{
-public:
-	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
-	{
-		int num1 = 0, num2 = 0;
-		int n = 0;
-		int m = 0;
-		while (l1->next)
-		{
-			num1 = num1 + l1->val * (pow(10,n));
-			l1 = l1->next;
-			n++;
-		}
-		while (l2->next)
-		{
-			num2 = num2 + l2->val * (pow(10, m));
-			l2 = l2->next;
-			m++;
-		}
-		int sum = num1 + num2;
-		cout << sum;
-		ListNode*ret = new ListNode(sum%10);
-		ListNode*tmp2 = ret;
-		sum /= 10;
-		while (sum)
-		{
-			//cout << sum % 10;
-			tmp2->next = new ListNode(sum % 10);
-			tmp2 = tmp2->next;
-			sum /= 10;
-		}
-		return ret;
-	}
-};
+//class Solution 
+//{
+//public:
+//	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+//	{
+//		int num1 = 0, num2 = 0;
+//		int n = 0;
+//		int m = 0;
+//		while (l1->next)
+//		{
+//			num1 = num1 + l1->val * (int)(pow(10, n) + 0.5);
+//			l1 = l1->next;
+//			n++;
+//		}
+//		while (l2->next)
+//		{
+//			num2 = num2 + l2->val *  (int)(pow(10, m) + 0.5);
+//			l2 = l2->next;
+//			m++;
+//		}
+//		int sum = num1 + num2;
+//		cout << sum;
+//		ListNode*ret = new ListNode(sum%10);
+//		ListNode*tmp2 = ret;
+//		sum /= 10;
+//		while (sum)
+//		{
+//			//cout << sum % 10;
+//			tmp2->next = new ListNode(sum % 10);
+//			tmp2 = tmp2->next;
+//			sum /= 10;
+//		}
+//		return ret;
+//	}
+//};
+
+ class Solution
+ {
+ public:
+	 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+	 {
+		 int tmpsum;
+		 int tmpnext;
+		 int num1;
+		 int num2;
+		 tmpsum = l1->val + l2->val;
+		 l1 = l1->next;
+		 l2 = l2->next;
+		 tmpnext = tmpsum / 10;
+		 ListNode*ret = new ListNode(tmpsum % 10);
+		 ListNode*tmp2 = ret;
+		 while ((l1 || l2||tmpnext))
+		 {
+			 if (l1)
+				 num1 = l1 -> val;
+			 else
+				 num1 = 0;
+			 if (l2)
+				 num2 = l2->val;
+			 else
+				 num2 = 0;
+			 tmpsum = num1 + num2 + tmpnext;
+			 tmpnext = 0;
+			 if(l1)
+			 l1 = l1->next;
+			 if(l2)
+			 l2 = l2->next;
+			 tmpnext = tmpsum / 10;
+			 tmp2->next = new ListNode(tmpsum % 10);
+			 tmp2 = tmp2->next;
+		 }
+		 return ret;
+	 }
+ };
 
 ListNode* create_list()
 {
@@ -61,8 +100,7 @@ ListNode* create_list()
 	{
 		tmpList = NULL;
 		head = NULL;
-	}
-		
+	}	
 	while (tmp != -1)
 	{
 		cin >> tmp;
@@ -78,14 +116,10 @@ ListNode* create_list()
 void ShowList(ListNode* list)
 {
 	ListNode * tmp = list;
-	while (1)
-	{
+	do {
 		cout << tmp->val;
 		tmp = tmp->next;
-		if (!tmp->next)
-			break;
-
-	}
+	} while (tmp->next);
 }
 int main()
 {
@@ -99,7 +133,7 @@ int main()
 	ListNode * ret = sol.addTwoNumbers(l1,l2);
 	cout << endl;
 	ShowList(ret);
-	
+	ShowList(ret);
 
 
 	/*cout << pow(10,2);*/
